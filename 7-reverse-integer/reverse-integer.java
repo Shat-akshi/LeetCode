6fe28c6b95d874146604491716093ea34941ccd4
Shatakshi <123860885+Shat-akshi@users.noreply.github.com>
2024-09-17 00:57:10 +0530

    Time: 1 ms (83.69%) | Memory: 40.8 MB (45.49%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -7,10 +7,10 @@ class Solution {
         {
             int d=temp%10;
             temp/=10;
-            if (sum > Integer.MAX_VALUE / 10 ) {
+            if (sum > Integer.MAX_VALUE / 10 || (sum == Integer.MAX_VALUE / 10 && d > 7)) {
                 return 0;  
             }
-            if (sum < Integer.MIN_VALUE / 10 ) {
+            if (sum < Integer.MIN_VALUE / 10 || (sum == Integer.MIN_VALUE / 10 && d < -8)) {
                 return 0;  
             }
 
