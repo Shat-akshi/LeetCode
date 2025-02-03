36d7b8f740248521373cf10a2a627486411f9c93
Shatakshi <123860885+Shat-akshi@users.noreply.github.com>
2025-02-04 02:23:50 +0530

    Time: 4 ms (27.84%) | Memory: 45.3 MB (95.23%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,33 +1,24 @@
 class Solution {
-    
-        public int candy(int[] ratings) {
-        int sum=0;
-        int[] a=new int[ratings.length];
-        for(int i=0;i<a.length;i++)
-        {
-            a[i]=1;
-        }
-        for(int i=0;i<ratings.length-1;i++)
-        {
-            if(ratings[i+1]>ratings[i])
-            {
-                a[i+1]=a[i]+1;
-            }
-        }
-        for(int i=ratings.length-1;i>0;i--)
-        {
-            if(ratings[i-1]>ratings[i])
-            {
-                if(a[i-1]<(a[i]+1))
-                {
-                    a[i-1]=a[i]+1;
-                }
-            }
-        }
-        for(int i=0;i<a.length;i++)
-        {
-            sum+=a[i];
-        }
-        return sum;
-    }
-    }
+  public int candy(int[] ratings) {
+    final int n = ratings.length;
+
+    int ans = 0;
+    int[] l = new int[n];
+    int[] r = new int[n];
+    Arrays.fill(l, 1);
+    Arrays.fill(r, 1);
+
+    for (int i = 1; i < n; ++i)
+      if (ratings[i] > ratings[i - 1])
+        l[i] = l[i - 1] + 1;
+
+    for (int i = n - 2; i >= 0; --i)
+      if (ratings[i] > ratings[i + 1])
+        r[i] = r[i + 1] + 1;
+
+    for (int i = 0; i < n; ++i)
+      ans += Math.max(l[i], r[i]);
+
+    return ans;
+  }
+}
