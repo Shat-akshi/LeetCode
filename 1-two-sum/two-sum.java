90104e0945dd85b7c3e3fd6627fc945f2da0404e
Shatakshi <123860885+Shat-akshi@users.noreply.github.com>
2025-10-24 01:20:56 +0530

    Time: 45 ms (29.68%) | Memory: 45.1 MB (32.58%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,18 +1,27 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] sol = new int[2];
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    sol[0]=i;
-                    sol[1]=j;
-                    break;
+        // int[] sol = new int[2];
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     for(int j=i+1;j<nums.length;j++)
+        //     {
+        //         if(nums[i]+nums[j]==target)
+        //         {
+        //             sol[0]=i;
+        //             sol[1]=j;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return sol;
+         for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
                 }
             }
         }
-        return sol;
+        // should never reach here as per problem statement
+        return new int[] {};
     }
 }
